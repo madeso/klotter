ac1c0974802dc689d272d6e962e5963b311a1356
Gustav <sir.gustav.the.coder@gmail.com>
2023-11-19 14:57:12 +0100

    Add a info category to logging

--- src/klotter/log.h
+++ src/klotter/log.h
@@ -1,4 +1,5 @@
 #pragma once
 
 #include "SDL_log.h"
+#define LOG_INFO(...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 #define LOG_ERROR(...) SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
