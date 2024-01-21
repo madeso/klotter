0acbbf1951fd2484adebbcaa3e026e5a2a87b691
Gustav <sir.gustav.the.coder@gmail.com>
2024-01-21 18:32:25 +0100

    Add todo

--- src/klotter/render/geom.extract.h
+++ src/klotter/render/geom.extract.h
@@ -18,8 +18,12 @@ using VertexVector = std::vector<float>;
 /// Extracted data from a Geom for a specific shader layout so a CompiledGeom can be created
 struct ExtractedGeom
 {
+	// todo(Gustav): replace with a byte buffer to support non-float types (like bone assignments)
 	VertexVector vertices;
+
+	// todo(Gustav): replace with stride
 	std::size_t floats_per_vertex;
+
 	std::vector<int> attribute_counts;
 	std::vector<u32> indices;
 	i32 face_size;
