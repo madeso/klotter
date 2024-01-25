91de6c1988f0168f6e4b58f56bfbfae87b50f41a
Gustav <sir.gustav.the.coder@gmail.com>
2024-01-25 23:17:25 +0100

    Add initial version of bitfield

---
+++ src/klotter/bitfield.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <type_traits>
+
+namespace klotter
+{
+
+template<typename T, typename I = std::underlying_type_t<T>>
+struct Bitfield
+{
+	T value;
+
+	Bitfield()
+		: value(static_cast<T>(0))
+	{
+	}
+
+	Bitfield(T t)
+		: value(t)
+	{
+	}
+
+	void set(T bit, bool new_value)
+	{
+		if (new_value)
+		{
+			value = static_cast<T>(static_cast<I>(value) | static_cast<I>(bit));
+		}
+		else
+		{
+			value = static_cast<T>(static_cast<I>(value) & ~static_cast<I>(bit));
+		}
+	}
+
+	void toggle(T bit)
+	{
+		value = static_cast<T>(static_cast<I>(value) ^ static_cast<I>(bit));
+	}
+
+	void clear()
+	{
+		value = static_cast<T>(0);
+	}
+
+	[[nodiscard]]
+	bool get(T bit) const
+	{
+		const I i = static_cast<I>(value) & static_cast<I>(bit);
+		return bit == static_cast<T>(i);
+	}
+
+	[[nodiscard]]
+	bool has_any_values() const
+	{
+		return value != static_cast<T>(0);
+	}
+};
+
+#define BIT_FIELD(x) (1 << (x + 1))
+
+}  //  namespace klotter
