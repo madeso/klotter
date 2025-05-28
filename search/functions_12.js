9b59cf2460ce43b91646d3b0667f0c43aab8887b
madeso <madeso@users.noreply.github.com>
2025-05-28 06:27:01 +0000

    deploy: 39845feeb78e04825057db23d4318328ffe08563

--- search/functions_12.js
+++ search/functions_12.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['with_5finstanced_5fmat4_0',['with_instanced_mat4',['../structklotter_1_1ShaderOptions.html#a7d0cecc1742fc49875515b8193589add',1,'klotter::ShaderOptions']]],
-  ['with_5fmsaa_1',['with_msaa',['../structklotter_1_1FrameBufferBuilder.html#abb13c3b37555723323e059b39814aa4a',1,'klotter::FrameBufferBuilder']]],
-  ['with_5ftransparent_5fcutoff_2',['with_transparent_cutoff',['../structklotter_1_1ShaderOptions.html#a824c6abd7ff4c7ebfbcc15364a68455c',1,'klotter::ShaderOptions']]],
-  ['write_5fobj_3',['write_obj',['../structklotter_1_1geom_1_1Builder.html#a37ee459d444ec29573ccee3240fa9a24',1,'klotter::geom::Builder']]]
+  ['with_5fdepth_0',['with_depth',['../structklotter_1_1FrameBufferBuilder.html#a6c5e2aabca52f9857946c55e7d4c52a8',1,'klotter::FrameBufferBuilder']]],
+  ['with_5finstanced_5fmat4_1',['with_instanced_mat4',['../structklotter_1_1ShaderOptions.html#a7d0cecc1742fc49875515b8193589add',1,'klotter::ShaderOptions']]],
+  ['with_5fmsaa_2',['with_msaa',['../structklotter_1_1FrameBufferBuilder.html#abb13c3b37555723323e059b39814aa4a',1,'klotter::FrameBufferBuilder']]],
+  ['with_5ftransparent_5fcutoff_3',['with_transparent_cutoff',['../structklotter_1_1ShaderOptions.html#a824c6abd7ff4c7ebfbcc15364a68455c',1,'klotter::ShaderOptions']]],
+  ['write_5fobj_4',['write_obj',['../structklotter_1_1geom_1_1Builder.html#a37ee459d444ec29573ccee3240fa9a24',1,'klotter::geom::Builder']]]
 ];
--- search/variables_8.js
+++ search/variables_8.js
@@ -3,10 +3,11 @@ var searchData=
   ['ib_0',['ib',['../structklotter_1_1LineBatch.html#a22adf043bc55df5ccdd1c10e680339e9',1,'klotter::LineBatch']]],
   ['id_1',['id',['../structklotter_1_1BaseTexture.html#aa0c1c5008e48db75b99c027cf8b80d08',1,'klotter::BaseTexture::id'],['../structklotter_1_1UniformBuffer.html#a7c2ed659d8fdcf1d15166343a4f8f1f7',1,'klotter::UniformBuffer::id']]],
   ['in_2',['in',['../structklotter_1_1CompiledCamera.html#af8f3aacb0e4384ae60db486c14dfdb60',1,'klotter::CompiledCamera']]],
-  ['index_3',['index',['../structklotter_1_1CompiledVertexElement.html#a9af8e87a9efdfcc2828d975379507f2a',1,'klotter::CompiledVertexElement::index'],['../structklotter_1_1CompiledVertexElementNoName.html#a8c274532efa2ebb3f9fa4c65c5e6df09',1,'klotter::CompiledVertexElementNoName::index']]],
-  ['indices_4',['indices',['../structklotter_1_1ExtractedGeom.html#a752875ea25c1a69d5b7905a7739defa2',1,'klotter::ExtractedGeom::indices'],['../structklotter_1_1CompiledVertexTypeList.html#afca053bc106f6c78d9dfb86f2e9825c9',1,'klotter::CompiledVertexTypeList::indices']]],
-  ['indigo_5',['indigo',['../namespaceklotter_1_1imgui.html#a7af53378e11ed2f4ec0c2ce54b6be954',1,'klotter::imgui']]],
-  ['instance_5fvbo_6',['instance_vbo',['../structklotter_1_1CompiledGeom__TransformInstance.html#ab5f7094925a36f1a915bbb22a51d6759',1,'klotter::CompiledGeom_TransformInstance']]],
-  ['instances_7',['instances',['../structklotter_1_1World.html#a42950d49b98dc3304ea9a25ddbf83c9f',1,'klotter::World']]],
-  ['is_5fgauss_8',['is_gauss',['../structklotter_1_1BlurOptions.html#a95c8cd4bd600737931412e2230899dfa',1,'klotter::BlurOptions']]]
+  ['include_5fdepth_3',['include_depth',['../structklotter_1_1FrameBufferBuilder.html#a132b280ba068b2247cc5d2a695bbcb16',1,'klotter::FrameBufferBuilder']]],
+  ['index_4',['index',['../structklotter_1_1CompiledVertexElement.html#a9af8e87a9efdfcc2828d975379507f2a',1,'klotter::CompiledVertexElement::index'],['../structklotter_1_1CompiledVertexElementNoName.html#a8c274532efa2ebb3f9fa4c65c5e6df09',1,'klotter::CompiledVertexElementNoName::index']]],
+  ['indices_5',['indices',['../structklotter_1_1ExtractedGeom.html#a752875ea25c1a69d5b7905a7739defa2',1,'klotter::ExtractedGeom::indices'],['../structklotter_1_1CompiledVertexTypeList.html#afca053bc106f6c78d9dfb86f2e9825c9',1,'klotter::CompiledVertexTypeList::indices']]],
+  ['indigo_6',['indigo',['../namespaceklotter_1_1imgui.html#a7af53378e11ed2f4ec0c2ce54b6be954',1,'klotter::imgui']]],
+  ['instance_5fvbo_7',['instance_vbo',['../structklotter_1_1CompiledGeom__TransformInstance.html#ab5f7094925a36f1a915bbb22a51d6759',1,'klotter::CompiledGeom_TransformInstance']]],
+  ['instances_8',['instances',['../structklotter_1_1World.html#a42950d49b98dc3304ea9a25ddbf83c9f',1,'klotter::World']]],
+  ['is_5fgauss_9',['is_gauss',['../structklotter_1_1BlurOptions.html#a95c8cd4bd600737931412e2230899dfa',1,'klotter::BlurOptions']]]
 ];
