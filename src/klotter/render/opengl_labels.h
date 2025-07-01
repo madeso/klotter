7d1d86e391d1119fd05b2c08de6d4c138b1a88c0
Gustav <sir.gustav.the.coder@gmail.com>
2025-07-01 20:37:58 +0200

    Remove unused debug  label arguments

--- src/klotter/render/opengl_labels.h
+++ src/klotter/render/opengl_labels.h
@@ -2,9 +2,7 @@
 
 #define DEBUG_LABEL_ARG_MANY const std::string& debug_label,
 #define DEBUG_LABEL_ARG_SINGLE const std::string &debug_label
-#define DEBUG_LABEL_EXPLICIT_SINGLE explicit
 #define DEBUG_LABEL_EXPLICIT_MANY
-#define SEND_DEBUG_LABEL(X) X
 #define SEND_DEBUG_LABEL_MANY(X) X,
 
 #define USE_DEBUG_LABEL(name) name
