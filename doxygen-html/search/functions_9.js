25d6e593e333ba50991713cadc65b96f10df9cc6
madeso <madeso@users.noreply.github.com>
2025-07-02 09:42:16 +0000

    deploy: 3e842377e705a54cefaedb333a621b96ffc364b7

--- doxygen-html/search/functions_9.js
+++ doxygen-html/search/functions_9.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['line_0',['line',['../structklotter_1_1LineBatch.html#aa9b4393b9e661a565f10309ef61fe405',1,'klotter::LineBatch']]],
   ['linebatch_1',['LineBatch',['../structklotter_1_1LineBatch.html#a3a43e334bd2c3ebad64c4e3e4af8ff90',1,'klotter::LineBatch::LineBatch(ShaderProgram *)'],['../structklotter_1_1LineBatch.html#a2a5f7384e73b6b919adb76aea2efe60c',1,'klotter::LineBatch::LineBatch(const LineBatch &amp;)=delete'],['../structklotter_1_1LineBatch.html#a964e2420c45c8f92bda5a4e41435fcf0',1,'klotter::LineBatch::LineBatch(LineBatch &amp;&amp;)=delete']]],
-  ['load_5fcubemap_5ffrom_5fcolor_2',['load_cubemap_from_color',['../group__texture.html#ga1935fc707e962dca5b1e45b935fa4a08',1,'klotter']]],
+  ['load_5fcubemap_5ffrom_5fcolor_2',['load_cubemap_from_color',['../group__texture.html#gab32ac44b53bbd24e8c9331dae86105d0',1,'klotter']]],
   ['load_5fcubemap_5ffrom_5fembedded_3',['load_cubemap_from_embedded',['../namespaceklotter.html#aa4142bff7e121332f50f6e6f409366f7',1,'klotter']]],
-  ['load_5fimage_5ffrom_5fcolor_4',['load_image_from_color',['../group__texture.html#gae7e52a0e0f2fb06556cb212c080f1c4c',1,'klotter']]],
+  ['load_5fimage_5ffrom_5fcolor_4',['load_image_from_color',['../group__texture.html#ga8da8502bdd8586fa0e76cf89c0629706',1,'klotter']]],
   ['load_5fimage_5ffrom_5fembedded_5',['load_image_from_embedded',['../namespaceklotter.html#a7564cf3dcd4da86f317f1faa37beb1f2',1,'klotter']]],
   ['load_5fshader_5fsource_6',['load_shader_source',['../namespaceklotter.html#a64c9c7c89ee622da0a56d5e0afc611de',1,'klotter']]],
   ['load_5fshaders_7',['load_shaders',['../group__render.html#gaae619e8aae48dd7405f07dfbca0e1505',1,'klotter']]],
