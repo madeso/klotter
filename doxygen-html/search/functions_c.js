bfd3c797c025751627631991c69dd25bee8b882c
madeso <madeso@users.noreply.github.com>
2025-07-18 21:55:15 +0000

    deploy: 89f3e71262023c8fa9236940741b53e0dc1620ce

--- doxygen-html/search/functions_c.js
+++ doxygen-html/search/functions_c.js
@@ -9,7 +9,7 @@ var searchData=
   ['rendererpimpl_6',['RendererPimpl',['../structklotter_1_1RendererPimpl.html#a5589ea5eb933cefc1fd719af2ae2f432',1,'klotter::RendererPimpl']]],
   ['rendersource_7',['RenderSource',['../structklotter_1_1RenderSource.html#a7bbdf727aee83637ea11695272907a31',1,'klotter::RenderSource::RenderSource()=default'],['../structklotter_1_1RenderSource.html#a1fbc3febe5e3d46202efebdac885dd5c',1,'klotter::RenderSource::RenderSource(const RenderSource &amp;)=delete'],['../structklotter_1_1RenderSource.html#ade2acc705758ca5458b8415f4e2716b3',1,'klotter::RenderSource::RenderSource(RenderSource &amp;&amp;)=delete']]],
   ['rendertexturewithshader_8',['RenderTextureWithShader',['../structklotter_1_1RenderTextureWithShader.html#aff7be13082c2aed0d2e2db9d77123b09',1,'klotter::RenderTextureWithShader']]],
-  ['renderworld_9',['RenderWorld',['../structklotter_1_1RenderWorld.html#a3f4a8e79693902c657c5c3145a04a0c3',1,'klotter::RenderWorld']]],
+  ['renderworld_9',['RenderWorld',['../structklotter_1_1RenderWorld.html#a72ee05fdb5ee6a4d82848f4da22d8048',1,'klotter::RenderWorld']]],
   ['replace_5fwith_5fsmooth_5fnormals_10',['replace_with_smooth_normals',['../structklotter_1_1geom_1_1Builder.html#a0040ee70080e791cf4a27d1450db0b0f',1,'klotter::geom::Builder']]],
   ['resolve_5fmultisampled_5fbuffer_11',['resolve_multisampled_buffer',['../group__texture.html#ga2db91a5f624e2dc351118f2d7b0ecf31',1,'klotter']]],
   ['result_12',['Result',['../structResult.html#a5314b530c0634f2fc8c0c464189a5abb',1,'Result::Result(T tt)'],['../structResult.html#aef1257385182062000e093cc7979f46f',1,'Result::Result(const E &amp;ee)']]],
