b0d87b8195858861fcd9f769685443ba3e58e2a9
madeso <madeso@users.noreply.github.com>
2025-08-23 10:05:23 +0000

    deploy: 00281fea224a611e4425c0aa9c9b67eaa225a7af

--- doxygen-html/search/variables_11.js
+++ doxygen-html/search/variables_11.js
@@ -5,11 +5,12 @@ var searchData=
   ['up_2',['UP',['../group__render.html#ga86f2208b879c568d1ee1e6e771c0caa1',1,'klotter']]],
   ['up_3',['up',['../structklotter_1_1CameraVectors.html#af53d2d059ea34390beb2f73311aa9e7e',1,'klotter::CameraVectors']]],
   ['use_5fblinn_5fphong_4',['use_blinn_phong',['../structklotter_1_1ShaderOptions.html#a82219aa56babe230c959a8715122a4e8',1,'klotter::ShaderOptions']]],
-  ['use_5fblur_5funiform_5',['use_blur_uniform',['../structklotter_1_1RealizeShader.html#a67c18653a5bd20289cb8f2040883713a',1,'klotter::RealizeShader']]],
-  ['use_5fhdr_6',['use_hdr',['../structklotter_1_1EffectStack.html#aeac7678584313570bb2642cd51a07759',1,'klotter::EffectStack::use_hdr'],['../structklotter_1_1RenderWorld.html#a5c1c9291f9d87e3fb1e80e93be6b0796',1,'klotter::RenderWorld::use_hdr']]],
-  ['use_5finstancing_7',['use_instancing',['../structklotter_1_1ShaderOptions.html#a17d6ad6992bb59a5003261b8565172db',1,'klotter::ShaderOptions']]],
-  ['use_5flights_8',['use_lights',['../structklotter_1_1ShaderOptions.html#aca2a16f5f74d2173ad16e3190ece623c',1,'klotter::ShaderOptions']]],
-  ['use_5ftexture_9',['use_texture',['../structklotter_1_1ShaderOptions.html#a5a82c2357b48240169bd49d29200c408',1,'klotter::ShaderOptions']]],
-  ['use_5ftransparency_10',['use_transparency',['../structklotter_1_1RenderContext.html#adfe2c953d814742376a8c64af73f0a26',1,'klotter::RenderContext']]],
-  ['uv_11',['uv',['../structklotter_1_1Vertex.html#a9c6e27934cc260af46556c60004edc43',1,'klotter::Vertex']]]
+  ['use_5fbloom_5',['use_bloom',['../structklotter_1_1RenderSettings.html#aba675c055c2e67f7622b35299261ebb8',1,'klotter::RenderSettings']]],
+  ['use_5fblur_5funiform_6',['use_blur_uniform',['../structklotter_1_1RealizeShader.html#a67c18653a5bd20289cb8f2040883713a',1,'klotter::RealizeShader']]],
+  ['use_5fhdr_7',['use_hdr',['../structklotter_1_1EffectStack.html#aeac7678584313570bb2642cd51a07759',1,'klotter::EffectStack::use_hdr'],['../structklotter_1_1RenderWorld.html#a5c1c9291f9d87e3fb1e80e93be6b0796',1,'klotter::RenderWorld::use_hdr']]],
+  ['use_5finstancing_8',['use_instancing',['../structklotter_1_1ShaderOptions.html#a17d6ad6992bb59a5003261b8565172db',1,'klotter::ShaderOptions']]],
+  ['use_5flights_9',['use_lights',['../structklotter_1_1ShaderOptions.html#aca2a16f5f74d2173ad16e3190ece623c',1,'klotter::ShaderOptions']]],
+  ['use_5ftexture_10',['use_texture',['../structklotter_1_1ShaderOptions.html#a5a82c2357b48240169bd49d29200c408',1,'klotter::ShaderOptions']]],
+  ['use_5ftransparency_11',['use_transparency',['../structklotter_1_1RenderContext.html#adfe2c953d814742376a8c64af73f0a26',1,'klotter::RenderContext']]],
+  ['uv_12',['uv',['../structklotter_1_1Vertex.html#a9c6e27934cc260af46556c60004edc43',1,'klotter::Vertex']]]
 ];
