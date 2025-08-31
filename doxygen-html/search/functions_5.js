e9bdaea000d353b48ed50f8b6c1b4f4c4a9df272
madeso <madeso@users.noreply.github.com>
2025-08-31 20:12:29 +0000

    deploy: cd48a1b02aba3da0c9949a1f6dce5c88061e2fb0

--- doxygen-html/search/functions_5.js
+++ doxygen-html/search/functions_5.js
@@ -1,14 +1,15 @@
 var searchData=
 [
   ['factoreffect_0',['FactorEffect',['../structklotter_1_1FactorEffect.html#a20c51552c66bf6c1e49d615a5021d4bd',1,'klotter::FactorEffect']]],
-  ['floatdragshaderprop_1',['FloatDragShaderProp',['../structklotter_1_1FloatDragShaderProp.html#a620fa2284c7fc037975a2fc20c90e18d',1,'klotter::FloatDragShaderProp']]],
-  ['floatslidershaderprop_2',['FloatSliderShaderProp',['../structklotter_1_1FloatSliderShaderProp.html#a594675312b454da4b4a06c9fcced5338',1,'klotter::FloatSliderShaderProp']]],
-  ['foa_5fcolor_3',['foa_color',['../structklotter_1_1geom_1_1Builder.html#a6cd975114cc2a208a083fcb7c3a52aed',1,'klotter::geom::Builder']]],
-  ['foa_5fnormal_4',['foa_normal',['../structklotter_1_1geom_1_1Builder.html#af706fa48387cd029fa3f8ce53fdb53a7',1,'klotter::geom::Builder']]],
-  ['foa_5fposition_5',['foa_position',['../structklotter_1_1geom_1_1Builder.html#ac84dda85ac9ca872b140777fca452c20',1,'klotter::geom::Builder']]],
-  ['foa_5ftext_5fcoord_6',['foa_text_coord',['../structklotter_1_1geom_1_1Builder.html#ad81e3f1140c8def587910b1564b0110e',1,'klotter::geom::Builder']]],
-  ['framebuffer_7',['FrameBuffer',['../structklotter_1_1FrameBuffer.html#a8450b449b540c024574013e1ead16f47',1,'klotter::FrameBuffer::FrameBuffer(unsigned int f, int w, int h)'],['../structklotter_1_1FrameBuffer.html#a09179c63fdfa5fd654c53e249d357da9',1,'klotter::FrameBuffer::FrameBuffer(const FrameBuffer &amp;)=delete'],['../structklotter_1_1FrameBuffer.html#ae9e61e8d6bc6040e77bae32b48fb34ac',1,'klotter::FrameBuffer::FrameBuffer(FrameBuffer &amp;&amp;)=delete']]],
-  ['framebufferbuilder_8',['FrameBufferBuilder',['../structklotter_1_1FrameBufferBuilder.html#a3e5157674b4f8d71cf2cef6c44ad0620',1,'klotter::FrameBufferBuilder']]],
-  ['frustumlightuniforms_9',['FrustumLightUniforms',['../structklotter_1_1FrustumLightUniforms.html#ab06a609169ec292dd41407b057b84127',1,'klotter::FrustumLightUniforms']]],
-  ['fullscreengeom_10',['FullScreenGeom',['../structklotter_1_1FullScreenGeom.html#a55753c215cb88a040da3e21b585f9d3a',1,'klotter::FullScreenGeom']]]
+  ['find_5fduplicates_1',['find_duplicates',['../group__vertex-layout.html#ga930a511a1263566c1155f47dd193e47c',1,'klotter']]],
+  ['floatdragshaderprop_2',['FloatDragShaderProp',['../structklotter_1_1FloatDragShaderProp.html#a620fa2284c7fc037975a2fc20c90e18d',1,'klotter::FloatDragShaderProp']]],
+  ['floatslidershaderprop_3',['FloatSliderShaderProp',['../structklotter_1_1FloatSliderShaderProp.html#a594675312b454da4b4a06c9fcced5338',1,'klotter::FloatSliderShaderProp']]],
+  ['foa_5fcolor_4',['foa_color',['../structklotter_1_1geom_1_1Builder.html#a6cd975114cc2a208a083fcb7c3a52aed',1,'klotter::geom::Builder']]],
+  ['foa_5fnormal_5',['foa_normal',['../structklotter_1_1geom_1_1Builder.html#af706fa48387cd029fa3f8ce53fdb53a7',1,'klotter::geom::Builder']]],
+  ['foa_5fposition_6',['foa_position',['../structklotter_1_1geom_1_1Builder.html#ac84dda85ac9ca872b140777fca452c20',1,'klotter::geom::Builder']]],
+  ['foa_5ftext_5fcoord_7',['foa_text_coord',['../structklotter_1_1geom_1_1Builder.html#ad81e3f1140c8def587910b1564b0110e',1,'klotter::geom::Builder']]],
+  ['framebuffer_8',['FrameBuffer',['../structklotter_1_1FrameBuffer.html#a8450b449b540c024574013e1ead16f47',1,'klotter::FrameBuffer::FrameBuffer(unsigned int f, int w, int h)'],['../structklotter_1_1FrameBuffer.html#a09179c63fdfa5fd654c53e249d357da9',1,'klotter::FrameBuffer::FrameBuffer(const FrameBuffer &amp;)=delete'],['../structklotter_1_1FrameBuffer.html#ae9e61e8d6bc6040e77bae32b48fb34ac',1,'klotter::FrameBuffer::FrameBuffer(FrameBuffer &amp;&amp;)=delete']]],
+  ['framebufferbuilder_9',['FrameBufferBuilder',['../structklotter_1_1FrameBufferBuilder.html#a3e5157674b4f8d71cf2cef6c44ad0620',1,'klotter::FrameBufferBuilder']]],
+  ['frustumlightuniforms_10',['FrustumLightUniforms',['../structklotter_1_1FrustumLightUniforms.html#ab06a609169ec292dd41407b057b84127',1,'klotter::FrustumLightUniforms']]],
+  ['fullscreengeom_11',['FullScreenGeom',['../structklotter_1_1FullScreenGeom.html#a55753c215cb88a040da3e21b585f9d3a',1,'klotter::FullScreenGeom']]]
 ];
