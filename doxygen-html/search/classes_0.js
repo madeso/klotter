574209f4d0d57fcbc0dd158300a586da8359b318
madeso <madeso@users.noreply.github.com>
2025-10-06 18:27:18 +0000

    deploy: f3f1482cfc2c29e11c2c361209faebdfb147e6bf

--- doxygen-html/search/classes_0.js
+++ doxygen-html/search/classes_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['aabb2_0',['AABB2',['../structVisualDebugging_1_1AABB2.html',1,'VisualDebugging']]],
-  ['app_1',['App',['../structklotter_1_1App.html',1,'klotter']]],
-  ['assets_2',['Assets',['../structklotter_1_1Assets.html',1,'klotter']]]
+  ['app_0',['App',['../structklotter_1_1App.html',1,'klotter']]],
+  ['assets_1',['Assets',['../structklotter_1_1Assets.html',1,'klotter']]]
 ];
