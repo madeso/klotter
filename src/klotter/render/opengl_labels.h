29488da3ceb86edaf91a1cb663f83f02ea3cad97
Gustav <sir.gustav.the.coder@gmail.com>
2026-01-02 13:13:39 +0100

    Document the label macros

--- src/klotter/render/opengl_labels.h
+++ src/klotter/render/opengl_labels.h
@@ -2,6 +2,10 @@
 
 #include "klotter/feature_flags.h"
 
+/** \addtogroup debug Debug
+ *  @{
+*/
+
 #if FF_HAS(ENABLE_GL_DEBUG)
 	#define DEBUG_LABEL_ARG_MANY const std::string& debug_label,
 	#define DEBUG_LABEL_ARG_SINGLE const std::string &debug_label
@@ -9,6 +13,7 @@
 	#define SEND_DEBUG_LABEL_MANY(X) X,
 
 	#define USE_DEBUG_LABEL(name) name
+
 	#define USE_DEBUG_LABEL_MANY(name) name,
 #else
 	#define DEBUG_LABEL_ARG_MANY
@@ -19,3 +24,29 @@
 	#define USE_DEBUG_LABEL(name)
 	#define USE_DEBUG_LABEL_MANY(name)
 #endif
+
+/// \def DEBUG_LABEL_ARG_MANY
+/// first debug label argument of many
+
+/// \def DEBUG_LABEL_ARG_SINGLE
+/// the only debug label argument (the function doesn't take any more rguments)
+
+/// \def DEBUG_LABEL_EXPLICIT_MANY
+/// optional explicit for when the constructor only takes one more argument and needs to be explicit if the label is removed
+
+/// \def SEND_DEBUG_LABEL_MANY
+/// forwards a label when there is more than one argument
+/// @param X The label to forward
+
+/// \def USE_DEBUG_LABEL
+/// use this label and there are no more arguments
+/// @param name the label
+
+/// \def USE_DEBUG_LABEL_MANY
+/// use this label and there are more arguments besides this one
+/// @param name the label
+
+
+/**
+ * @}
+*/
