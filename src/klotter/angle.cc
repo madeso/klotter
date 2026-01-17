fff2e3545120d95b5876459d72e21afcd9addd88
Gustav <sir.gustav.the.coder@gmail.com>
2026-01-17 13:58:55 +0100

    Add angle type from euphoria

---
+++ src/klotter/angle.cc
@@ -0,0 +1,212 @@
+#include "klotter/angle.h"
+#include "assert.h"
+
+#include <cmath>
+
+namespace klotter
+{
+	/// Returns a value that wraps around the range_
+	/// When the value go outside the range, the value is wrapped back to either the start if exited at the end or vice versa.
+	template<typename T>
+	T wrap(T range_lower_bound, T value, T range_upper_bound)
+	{
+		const T diff = range_upper_bound - range_lower_bound;
+		ASSERT(diff > 0);
+		T wrapped = value - range_lower_bound;
+		while (wrapped < 0)
+		{
+			wrapped += diff;
+		}
+		while (wrapped > diff)
+		{
+			wrapped -= diff;
+		}
+		return range_lower_bound + wrapped;
+	}
+
+    void
+    Angle::wrap()
+    {
+		radians = klotter::wrap(0.0f, radians, pi * 2.0f);
+    }
+
+
+    void
+    Angle::operator+=(const Angle& rhs)
+    {
+        radians += rhs.radians;
+    }
+
+
+    void
+    Angle::operator-=(const Angle& rhs)
+    {
+        radians -= rhs.radians;
+    }
+
+
+    void
+    Angle::operator*=(float rhs)
+    {
+        radians *= rhs;
+    }
+
+
+    void
+    Angle::operator/=(float rhs)
+    {
+        radians /= rhs;
+    }
+
+
+    Angle
+    Angle::operator-() const
+    {
+        return Angle::from_radians(-radians);
+    }
+
+
+    float
+    sin(const Angle& ang)
+    {
+        return std::sin(ang.as_radians());
+    }
+
+
+    float
+    cos(const Angle& ang)
+    {
+        return std::cos(ang.as_radians());
+    }
+
+
+    float
+    tan(const Angle& ang)
+    {
+        return std::tan(ang.as_radians());
+    }
+
+
+    Angle
+    asin(float v)
+    {
+        ASSERT(v <= 1 && "v must be smaller than 1");
+        ASSERT(v >= -1 && "v must be greater than -1");
+        return Angle::from_radians(std::asin(v));
+    }
+
+
+    Angle
+    acos(float v)
+    {
+        ASSERT(v <= 1 && "v must be smaller than 1");
+        ASSERT(v >= -1 && "v must be greater than -1");
+        return Angle::from_radians(std::acos(v));
+    }
+
+
+    Angle
+    atan(float v)
+    {
+        return Angle::from_radians(std::atan(v));
+    }
+
+
+    Angle
+    atan2(float y, float x)
+    {
+        return Angle::from_radians(std::atan2(y, x));
+    }
+
+
+    Angle
+    Angle::get_wrapped() const
+    {
+        Angle temp = *this;
+        temp.wrap();
+        return temp;
+    }
+
+
+    Angle
+    operator+(const Angle& lhs, const Angle& rhs)
+    {
+        Angle temp(lhs);
+        temp += rhs;
+        return temp;
+    }
+
+
+    Angle
+    operator-(const Angle& lhs, const Angle& rhs)
+    {
+        Angle temp(lhs);
+        temp -= rhs;
+        return temp;
+    }
+
+
+    Angle
+    operator/(const Angle& lhs, float rhs)
+    {
+        Angle temp(lhs);
+        temp /= rhs;
+        return temp;
+    }
+
+
+    Angle operator*(const Angle& lhs, float rhs)
+    {
+        Angle temp(lhs);
+        temp *= rhs;
+        return temp;
+    }
+
+    Angle operator*(float rhs, const Angle& lhs)
+    {
+        return lhs * rhs;
+    }
+
+    bool
+    operator<(const Angle& lhs, const Angle& rhs)
+    {
+        return lhs.as_radians() < rhs.as_radians();
+    }
+
+
+    bool
+    operator<=(const Angle& lhs, const Angle& rhs)
+    {
+        return lhs.as_radians() <= rhs.as_radians();
+    }
+
+
+    bool
+    operator>(const Angle& lhs, const Angle& rhs)
+    {
+        return lhs.as_radians() > rhs.as_radians();
+    }
+
+
+    bool
+    operator>=(const Angle& lhs, const Angle& rhs)
+    {
+        return lhs.as_radians() >= rhs.as_radians();
+    }
+
+
+    Angle lerp_angle(const Angle& from, float v, const Angle& to)
+    {
+        // https://gamedev.stackexchange.com/a/72364
+        const auto dtheta = to - from;
+
+        const auto new_from = dtheta > half_turn
+            ? from + one_turn
+            : dtheta < -half_turn
+                ? from - one_turn
+                : from
+            ;
+        
+        return from + ( to - new_from ) * v;
+    }
+}
---
+++ src/klotter/angle.h
@@ -0,0 +1,87 @@
+#pragma once
+
+namespace klotter
+{
+
+constexpr float pi = 3.14159265359f;
+
+/// An angle in both degrees and radians.
+struct Angle
+{
+    [[nodiscard]] constexpr static Angle
+    from_degrees(float degrees)
+    {
+        return Angle(rad_from_deg(degrees));
+    }
+
+    [[nodiscard]] constexpr static Angle
+    from_radians(float radians)
+    {
+        return Angle(radians);
+    }
+
+    void wrap();
+
+
+    [[nodiscard]] constexpr float
+    as_degrees() const
+    {
+        return deg_from_rad(radians);
+    }
+
+    [[nodiscard]] constexpr float
+    as_radians() const
+    {
+        return radians;
+    }
+
+    [[nodiscard]] Angle get_wrapped() const;
+
+    void operator+=(const Angle& rhs);
+    void operator-=(const Angle& rhs);
+    void operator*=(float rhs);
+    void operator/=(float rhs);
+    Angle operator-() const;
+
+private:
+    float radians;
+
+    constexpr explicit Angle(float r) : radians(r) {}
+
+    [[nodiscard]] static constexpr float
+    deg_from_rad(float radians)
+    {
+        return (180.0f / pi) * radians;
+    }
+
+    [[nodiscard]] static constexpr float
+    rad_from_deg(float degrees)
+    {
+        return pi / 180.0f * degrees;
+    }
+};
+
+constexpr Angle one_turn = Angle::from_radians(pi * 2.0f);
+constexpr Angle half_turn = Angle::from_radians(pi);
+constexpr Angle quarter_turn = Angle::from_radians(pi / 2.0f);
+constexpr Angle no_rotation = Angle::from_radians(0.0f);
+
+float sin(const Angle& ang);
+float cos(const Angle& ang);
+float tan(const Angle& ang);
+Angle asin(float v);
+Angle acos(float v);
+Angle atan(float v);
+Angle atan2(float y, float x);
+
+Angle operator+(const Angle& lhs, const Angle& rhs);
+Angle operator-(const Angle& lhs, const Angle& rhs);
+Angle operator*(const Angle& lhs, float rhs);
+Angle operator/(const Angle& lhs, float rhs);
+Angle operator*(float rhs, const Angle& lhs);
+
+bool operator<(const Angle& lhs, const Angle& rhs);
+bool operator<=(const Angle& lhs, const Angle& rhs);
+bool operator>(const Angle& lhs, const Angle& rhs);
+bool operator>=(const Angle& lhs, const Angle& rhs);
+}
