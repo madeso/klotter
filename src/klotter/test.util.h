8727fecb4631927ead0ec69a8c19377c228e6f3f
Gustav <sir.gustav.the.coder@gmail.com>
2026-03-12 10:55:24 +0100

    Add untested string diff

--- src/klotter/test.util.h
+++ src/klotter/test.util.h
@@ -59,6 +59,21 @@ struct Equaler
 		});
 	}
 
+	Self& add(const std::string& name, std::string (*convert)(const T&))
+	{
+		return impl(name, convert, [](const std::string& l, const std::string& r)
+		{
+			const auto same = l == r;
+			if (same) return catchy::FalseString::True();
+			std::ostringstream ss;
+			const auto edit = levenshtein(l, r);
+			const auto diff_index = find_index_of_first_diff(l, r);
+			ss << "length diff " << l.length() << " vs " << r.length() << ", changes=" << edit << ", first diff at "
+			   << diff_index << " (" << l[diff_index] << " vs " << r[diff_index] << ")";
+			return catchy::FalseString::False(ss.str());
+		});
+	}
+
 	template<typename C, typename F>
 	Self& add(const std::string& name, const C& convert, F min)
 	{
@@ -87,6 +102,55 @@ struct Equaler
 		}
 
 		return catchy::FalseString::False(ss.str());
+	}
+
+	// mit from https://github.com/guilhermeagostinelli/levenshtein/blob/master/levenshtein.cpp
+	static int levenshtein(const std::string& foo, const std::string& bar) {
+		const auto foo_size = foo.size();
+		const auto bar_size = bar.size();
+
+		std::size_t state[foo_size + 1][bar_size + 1];
+		
+		if (foo_size == 0)
+			return bar_size;
+		if (bar_size == 0)
+			return foo_size;
+
+		for (std::size_t foo_index = 0; foo_index <= foo_size; foo_index++)
+		{
+			state[foo_index][0] = foo_index;
+		}
+
+		for (std::size_t bar_index = 0; bar_index <= bar_size; bar_index++)
+		{
+			state[0][bar_index] = bar_index;
+		}
+
+		for (std::size_t foo_index = 1; foo_index <= foo_size; foo_index++)
+		{
+			for (int bar_index = 1; bar_index <= bar_size; bar_index++)
+			{
+				const auto change_cost = (bar[bar_index - 1] == foo[foo_index - 1]) ? 0 : 1;
+
+				const auto cost_foo = state[foo_index - 1][bar_index] + 1;
+				const auto cost_bar = state[foo_index][bar_index - 1] + 1;
+				const auto cost_both = state[foo_index - 1][bar_index - 1] + change_cost;
+
+				const auto min_cost = std::min(std::min(cost_foo, cost_bar), cost_both);
+				state[foo_index][bar_index] = min_cost;
+			}
+		}
+		
+		return state[foo_size][bar_size];
+	}
+
+	static std::size_t find_index_of_first_diff(const std::string& first, const std::string& second) {
+		std::size_t index = 0;
+		for (; index <= std::min(first.length(), second.length()); index++)
+		{
+			if(first[index] != second[index]) return index;
+		}
+		return index;
 	}
 };
 
